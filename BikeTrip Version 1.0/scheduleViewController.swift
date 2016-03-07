ccc1cb7fba5aadcc2abcb7ab9e7b59bd2f969381
ss42 <you@example.com>
2016-03-06 18:08:41 -0800

    table view works

--- BikeTrip Version 1.0/scheduleViewController.swift
+++ BikeTrip Version 1.0/scheduleViewController.swift
@@ -9,53 +9,65 @@
 import UIKit
 
 class scheduleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-    @IBOutlet weak var textLabel: UILabel!
-
-   
-    @IBOutlet weak var tableView: UITableView!
+ 
+    @IBOutlet weak var scheduleTableView: UITableView!
+    
+    //var objects: NSMutableArray! = NSMutableArray()
+    
+    
+    
     var animalNames: [String] = ["Cat","Dog","Elephant","Gangaroo","Hipopotamos","Lion","Zebra"];
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       // datePicker.addTarget(self, action: Selector("dataPickerChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+       
         
         
-        // Do any additional setup after loading the view.
+        //self.scheduleTableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        
+        
+        return animalNames.count
+    }
+    
+    
+   
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let myCell  = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "myCell")
+        //let myCell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as! TableViewCell
+        myCell.textLabel!.text = animalNames[indexPath.row]
+        //myCell.titleLabel!.text = self.objects[0]
+       
+        
+        
+        //myCell.testButton.tag = indexPath.row   //testbutton needs to be renamed
+        //myCell.testButton.addTarget(self, action: "logAction", forControlEvents: .TouchUpInside)
+        
+        //myCell.textLabel!.textColor = UIColor.darkTextColor()
+        return myCell
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        self.performSegueWithIdentifier("showDetailSegue", sender: self)
+    }
+    
+
+    
+
     
     
     @IBAction func submitPressed(sender: AnyObject) {
     }
+    
+    
 
-    func datePickerChanged(datePicker:UIDatePicker) {
-        var dateFormatter = NSDateFormatter()
-        
-        dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle
-        dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
-        
-        var strDate = dateFormatter.stringFromDate(datePicker.date)
-        //dateLabel.text = strDate
-    }
-   
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        return animalNames.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-    {
-        let cell:UITableViewCell=UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "mycell")
-        cell.textLabel!.text = animalNames[indexPath.row]
-        //var image : UIImage = UIImage(named: animalImages[indexPath.row])!
-        //cell.imageView.image = image
-        return cell
-    }
     /*
     // MARK: - Navigation
 
